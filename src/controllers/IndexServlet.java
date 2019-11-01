806316898cad4334c522176200c1ae87befcf966
makoto.michishita <11t6041fbiz@gmail.com.jp>
2019-11-01 15:37:14 +0900

    Add database files

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class IndexServlet
  */
-@WebServlet(name = "/index", urlPatterns = { "//index" })
+@WebServlet("/index")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
